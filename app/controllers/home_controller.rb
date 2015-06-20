7e9ba76e6e835d169a7bd6befc3f29fa7adce139
Rommel Castro <rommelxcastro@gmail.com>
2015-06-20 09:04:46 -0700

    views stuff

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def temp
+  	redirect_to root_path
+  end
+
+  def question
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
+  post '/answers' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
